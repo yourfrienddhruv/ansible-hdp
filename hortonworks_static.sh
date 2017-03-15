4fb3e3b82b0f708520b2a7e0d150eda1b5c508df
yourfrienddhruv <yourfrienddhruv@gmail.com>
2017-03-15 13:59:42 +0530

    remove cdh options

--- hortonworks_static.sh
+++ hortonworks_static.sh
@@ -3,4 +3,4 @@
 VARS="${VARS} ANSIBLE_SCP_IF_SSH=y ANSIBLE_HOST_KEY_CHECKING=False"
 
 export $VARS
-ansible-playbook -f 20 -i inventory/static playbooks/hortonworks.yml
+ansible-playbook -v --private-key ~/github/ambari-vagrant/centos6.4/insecure_private_key -f 20 -i inventory/static playbooks/hortonworks.yml
