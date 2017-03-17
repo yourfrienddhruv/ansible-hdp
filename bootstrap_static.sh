475f5ca34f1615828e48c7b99f2f106c938c48ce
yourfrienddhruv <yourfrienddhruv@gmail.com>
2017-03-17 19:42:26 +0530

    ambari non-root server install

--- bootstrap_static.sh
+++ bootstrap_static.sh
@@ -3,4 +3,4 @@
 VARS="${VARS} ANSIBLE_SCP_IF_SSH=y ANSIBLE_HOST_KEY_CHECKING=False"
 
 export $VARS
-ansible-playbook -v -f 20 -i inventory/static --private-key ~/github/ambari-vagrant/centos6.4/insecure_private_key playbooks/bootstrap.yml
+ansible-playbook  -f 20 -i inventory/static --private-key ~/github/ambari-vagrant/centos6.4/insecure_private_key playbooks/bootstrap.yml
